17b599dc378d4a0ab6c716c316c6e867b5a990ee
Abdulaziz Almoallim <aziz.almoallim@gmail.com>
2020-01-19 07:12:07 -0500

    Completed decoded of CSV file
    decode CSV file for AIS

--- Parsing2_0.py
+++ Parsing2_0.py
@@ -2,13 +2,17 @@ import pandas as pd
 import numpy as np 
 import pyAISm
 
-raw_data = pd.read_csv("CCG_AIS_Log_2018-05-01.csv", sep="\\")
+raw_data = pd.read_csv("CCG_AIS_Log_2018-05-01.csv" ,sep="\\")
 # print(raw_data)
 df = raw_data.iloc[:,2]
-# print(df)
-# df_str = df.astype(str)
-# print(df_str)
+print(df)
 
-ais_data = df.apply(lambda x: x.pyAISm.decod_ais(df))
-# ais_format = pyAISm.format_ais(ais_data)
-# print(ais_format)
+data = pd.DataFrame(columns=['type','repeat','mmsi','status','turn','speed','knots','accuracy','lon','lat','course','heading','second','maneuver','raim','radio']) 
+for row in df:
+	if type(row) == float:
+		pass
+	else:
+		decoded = pyAISm.decod_ais(row)
+		formating = pyAISm.format_ais(decoded)
+		data.append(formating, ignore_index=True)
+decoded_data = data.to_csv("CCG_AIS_Log_2018-05-01_DECODED.csv")
